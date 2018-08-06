b17bc85f96ab4511c3bd724dad766fc2e355ad4a
vijayrana-eits <vijay.rana@essorsolutions.net>
2018-08-06 17:30:30 +0530

    2nd time jenkin test file

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+kdnvfvnfvkregvre ;
 echo "i am here"."<br />";
-for ($x = 0; $x <= 10; $x++) {
+for ($x ;= 0; $x <= 10; $x++) {
     echo "The number is: $x "."<br />";
 } 
 echo "test";
+xvdvgrg
