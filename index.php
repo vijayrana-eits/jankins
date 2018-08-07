0146cc9365200822a9555c0dcd1d7cdbe6f0c151
vijayrana-eits <vijay.rana@essorsolutions.net>
2018-08-07 15:07:54 +0530

    2nd time jenkin test file

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-kdnvfvnfvkregvre ;
+error_reporting(1);
 echo "i am here"."<br />";
-for ($x ;= 0; $x <= 10; $x++) {
+for ($x = 0; $x <= 10; $x++) {
     echo "The number is: $x "."<br />";
 } 
 echo "test";
-xvdvgrg
+?>
